6c64ea7bbe71b1054bfb8654fae752dce0f499c1
Efjay <efjayengelbrecht@gmail.com>
2025-11-29 08:12:02 +0200

    simplicity is key.
    reverted tback to standard calculator , less noise
    and less code.
    
    fully functional.
    
    Keyboard support to be updated on next patch.

--- script.js
+++ script.js
@@ -1,62 +1,21 @@
-let firstNumber = '';
-let secondNumber = '';
-let operator = '';
-let displayValue = '0';
 
-function updateToDisplay () {
-    document.getElementById('display').textContent = displayValue;
+// Calculator Program
+
+const display = document.getElementById('display');
+
+function appendToDisplay(input) {
+    display.value += input;
 }
 
-function appendChar(char) {
-    if (displayValue === '0' || displayValue === firstNumber) {
-        displayValue = number;
-    } else {
-        displayValue += number;
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
     }
-    updateToDisplay();
-}
-
-function setOperator(op) {
-    if (firstNumber === '') {
-        firstNumber = displayValue;
-    } else if (secondNumber === '') {
-        secondNumber = displayValue;
-        firstNumber = operate(parseFloat(firstNumber), parseFloat(secondNumber), operator);
-        displayValue = firstNumber;
-        updateDisplay();
-        secondNumber = '';
+    catch(error){
+        display.value = "Error";
     }
-    operator = op;
-    displayValue = firstNumber;
-    updateDisplay();
-}
-
-function calculate() {
-    if (firstNumber !== '' && operator !== '' && displayValue !== '') {
-        secondNumber = displayValue;
-        const result = operate(parseFloat(firstNumber), parseFloat(secondNumber), operator);
-        displayValue = result;
-        updateDisplay();
-        firstNumber = result;
-        secondNumber = '';
-        operator = '';
-    }
-}
-
-function operate(num1, num2, op) {
-    switch (op) {
-        case '+':
-            return num1 + num2;
-        case '-':
-            return num1 - num2;
-        case '*':
-            return num1 * num2;
-        case '/':
-            if (num2 === 0) return 'Error';
-            return num1 / num2;
-        default:
-            return 0;
-    }
-}
-
-// Logic updated , needs to correct syntax to work with my current HTML tags
+} 
